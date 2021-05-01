9fb1f9e9a13e938f9331d9028fca241f91d3dad0
Kyle Johnson <kylesworkshopp@hotmail.com>
2021-05-01 14:19:44 -0700

    Let's Try Something New

--- src/main.cpp
+++ src/main.cpp
@@ -1,87 +1,32 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#include <iostream>
-#include <string>
-#include <vector>
 
-#include "../include/Entity.hpp"
-#include "../include/RenderWindow.hpp"
-#include "../include/Math.hpp"
-#include "../include/Utils.hpp"
-
-template <typename T> using d_array = std::vector<T>; // alias std::vector
+#include "../include/Game.hpp"
 
 using namespace std;
 
 int main(int argc, char* args[])
 {
-    // INITIALIZE
-    if (SDL_Init(SDL_INIT_VIDEO) > 0)
-        cout << "SDL_Init failed. Error: " << SDL_GetError() << endl;
-    else if (!IMG_Init(IMG_INIT_PNG))
-        cout << "IMG_Init failed. Error: " << SDL_GetError() << endl;
-    else
-    // ON START
+    const int maxFramesPerSecond = 60;
+    const int minMilisPerFrame = 1000 / maxFramesPerSecond;
+    Uint32 frameStartTimeInMilis;
+    int frameTimeLengthInMilis;
+
+    Game* game = new Game();
+    game->init("GAME", 640, 480);
+    while (game->running())
     {
-        RenderWindow window("Game", 640, 480);
-        cout << "Max FPS for Window: " << window.getRefreshRate() << endl;
+        frameStartTimeInMilis = SDL_GetTicks();
 
-        SDL_Texture* sheetTexture =
-            window.loadTexture("res/images/goldenboy-sheet.png");
-        // Entity block(16,16,sheetTexture);
+        game->handleEvents();
+        game->update();
+        game->render();
 
-        d_array<Entity> entities = {
-            Entity(Vector(16, 16), sheetTexture),
-            Entity(Vector(16, 32), sheetTexture),
-        };
-
-        bool gameRunning = true;
-        SDL_Event event;
-
-        const float timeStep = 0.01f;
-        float accumulator = 0.0f;
-        float currentTime = utils::hireTimeInSeconds();
-
-        // GAME LOOP
-        while (gameRunning)
-        {
-            int startTicks = SDL_GetTicks();
-
-            float newTime = utils::hireTimeInSeconds();
-            float frameTime = newTime - currentTime;
-            currentTime = newTime;
-            accumulator += frameTime;
-
-            while (accumulator >= timeStep)
-            {
-                accumulator -= timeStep;
-            }
-            const float alpha = accumulator / timeStep;
-
-            while (SDL_PollEvent(&event))
-                if (event.type == SDL_QUIT) gameRunning = false;
-
-            // RENDER
-            window.clear();
-
-            for (Entity &e : entities)
-                window.render(e);
-
-            window.display();
-
-            int frameTicks = SDL_GetTicks() - startTicks;
-            int msPerFrame = (1000 / window.getRefreshRate());
-            if (frameTicks < msPerFrame)
-                SDL_Delay(msPerFrame - frameTicks);
-        }
-
-        // ON END
-        SDL_DestroyTexture(sheetTexture);
-        sheetTexture = NULL;
-
-        // CLEANUP
-        window.cleanUp();
+        frameTimeLengthInMilis = SDL_GetTicks() - frameStartTimeInMilis;
+        if (minMilisPerFrame > frameTimeLengthInMilis)
+            SDL_Delay(minMilisPerFrame - frameTimeLengthInMilis);
     }
-    SDL_Quit();
+    game->clean();
+
     return 0;
 }
